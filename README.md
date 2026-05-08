8d3d0f8990a89f106dff175ce588d4335017ddfb
Gokul R <gokulrajeshkumar1063@gmail.com>
2026-05-08 22:42:14 +0530

    First commit
    This verison has the seed for the contaier, and other basics

---
+++ README.md
@@ -0,0 +1,19 @@
+# Backend Engineering Assignment
+
+Tech Stack:
+- Java 17
+- Spring Boot 3
+- PostgreSQL
+- Redis
+
+Phase 1:
+- Post APIs
+- Comment APIs
+- Like APIs
+
+Setup:
+1. Run Docker:
+   docker compose up -d
+
+2. Run Spring Boot:
+   mvn spring-boot:run
---
+++ src/main/java/com/assignment/backend/BackendEngineeringAssignmentApplication.java
@@ -0,0 +1,12 @@
+package com.assignment.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendEngineeringAssignmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendEngineeringAssignmentApplication.class, args);
+    }
+}
